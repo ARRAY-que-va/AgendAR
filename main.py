08437ce0a6421bc15941d0279c905633fb61e254
Exequiel <exequiel.vera.95@gmail.com>
2025-06-18 13:18:27 -0300

    Se agrego la función de crear usuarios

--- main.py
+++ main.py
@@ -50,6 +50,49 @@ def login():
         print("Credenciales incorrectas.\n")
         return None
 
+# FUNCIÓN: crear_cuenta
+def crear_usuario(conn, cursor): 
+    print("\n=== Creación de Cuenta ===")
+    nuevo_usuario = input("Define tu nombre de usuario: ")
+    nueva_contrasena = input("Define tu contraseña: ")
+    rol_defecto = "paciente"
+    try:
+        cursor.execute("SELECT id_user FROM user WHERE usuario = %s", (nuevo_usuario,))
+        if cursor.fetchone():
+            print("El nombre de usuario ya existe. Por favor, elige otro.")
+            return
+        cursor.execute("INSERT INTO user (usuario, pwd, rol) VALUES (%s, %s, %s)", (nuevo_usuario, nueva_contrasena, rol_defecto))
+        conn.commit() 
+        print("Cuenta de usuario creada con éxito.")
+        cursor.execute("SELECT LAST_INSERT_ID()")
+        id_usuario_reciente = cursor.fetchone()[0]
+        if rol_defecto == "paciente":
+            nombre_paciente = input("Tu nombre: ")
+            apellido_paciente = input("Tu apellido: ")
+            dni_paciente = input("Tu DNI: ") 
+            email_paciente = input("Tu email (opcional): ")
+            telefono_paciente = input("Tu teléfono (opcional): ")
+            direccion_paciente = input("Tu dirección: ")
+            cursor.execute("SELECT id_obrasocial, nombre FROM obrasocial")
+            obras_sociales_disponibles = cursor.fetchall()
+            print("\nObras Sociales disponibles:")
+            for os_id, os_nombre in obras_sociales_disponibles:
+                print(f"{os_id}. {os_nombre}")
+            obrasocial_id_str = input("Selecciona el ID de tu obra social (dejar vacío si no tienes): ")
+            obrasocial_id = int(obrasocial_id_str) if obrasocial_id_str.isdigit() else None
+            cursor.execute("""
+                INSERT INTO pacientes (nombre, apellido, user_id, dni, fecha_nacimiento, telefono, email, obrasocial_id, direccion)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (nombre_paciente, apellido_paciente, id_usuario_reciente, dni_paciente, None, telefono_paciente, email_paciente, obrasocial_id, direccion_paciente))
+            conn.commit() 
+            print("Datos de paciente guardados con éxito.")
+    except mysql.connector.Error as err:
+        print(f"Error al crear la cuenta: {err}")
+        if conn:
+            conn.rollback() 
+    except Exception as e:
+        print(f"Ocurrió un error inesperado: {e}")
+
 # falta hacer la funcion def crear cuenta  
 iniciar_sistema()
 
